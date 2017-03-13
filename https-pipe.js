0d655edde6105dd15f2cf63dc9aeda48a3c826ce
Brenton Holswich <brenton.holswich@gmail.com>
2017-03-13 07:31:30 +0000

    we are now listening for the `data` event to fire and console.logging it when it comes through

--- https-pipe.js
+++ https-pipe.js
@@ -3,14 +3,20 @@ let https = require('https');
 
 let options = {
   host: 'www.example.org',
-  path:'/'
+  path: '/'
 };
 
 //  Create a callback to use once the request is made
 let cb = (response) => {
   console.log('In response handler callback');
+
+  response.on('data', (chunk) => {
+    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+    console.log(chunk.toString());
+  });
   console.log('Response:', response);
 };
+
 
 console.log("I'm about to make the request!");
 
